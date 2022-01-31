5351636201e07c0f42ac067d0128e84a91aeb080
Nilton Jesus Rivadeneyra Llerena <nilton.rivadeneyra@softtek.com>
2022-01-31 09:53:40 -0500

    ssl

--- src/main/java/com/pokemon/api/util/RestTemplateServiceImpl.java
+++ src/main/java/com/pokemon/api/util/RestTemplateServiceImpl.java
@@ -8,6 +8,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import org.apache.http.ssl.TrustStrategy;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 @Service
 public class RestTemplateServiceImpl implements IRestTemplateService {
 
@@ -16,12 +28,37 @@ public class RestTemplateServiceImpl implements IRestTemplateService {
 
 		ResponseEntity<?> responsePokemonEntity = null;
 
-		RestTemplate restTemplate = new RestTemplate();
+		try {
 
-		HttpEntity<Object> entity = new HttpEntity<Object>(requestBody, headers());
+			TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+				@Override
+				public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+					return true;
+				}
+			};
 
-		responsePokemonEntity = restTemplate.exchange(url, method, entity, Object.class);
+			SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+					.loadTrustMaterial(null, acceptingTrustStrategy).build();
 
+			SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+			requestFactory.setConnectTimeout(20000);
+			requestFactory.setConnectionRequestTimeout(20000);
+			requestFactory.setReadTimeout(30000);
+			requestFactory.setHttpClient(httpClient);
+			
+			RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+			HttpEntity<Object> entity = new HttpEntity<Object>(requestBody, headers());
+
+			responsePokemonEntity = restTemplate.exchange(url, method, entity, Object.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return responsePokemonEntity;
 	}
 
